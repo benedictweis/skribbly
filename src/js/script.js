5ee6457b9823931feba30567d2ea5988ebbf8699
Manugamez <47077777+Manugamez@users.noreply.github.com>
2022-05-19 09:32:34 +0200

    added drawing in the canvas (WIP)

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,32 @@
 let canvas = document.querySelector(".canvas");
 let canvas_container = document.querySelector(".canvas-container");
 
+// size of canvas relative to actual size on screen
+let relativeSize;
+
+canvas.height = 800;
+canvas.width = 1200;
+
 resize_canvas();
 window.addEventListener("resize", () => {
     resize_canvas();
 })
 
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => drawing = true);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => drawing = false);
+canvas.addEventListener("mouseout", () => drawing = false);
+
 let ctx = canvas.getContext("2d");
 
-canvas.height = 800;
-canvas.width = 1200;
+// size of circles drawn
+let brushSize = 10;
 
 ctx.imageSmootingEnabled = false;
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+
+draw(200,200);
 
 function resize_canvas() {
     if (canvas_container.clientWidth / canvas_container.clientHeight < 3/2) {
@@ -24,5 +36,19 @@ function resize_canvas() {
     else {
         canvas.style.height = "100%";
         canvas.style.width = "";
+    }
+    recalculateRelativeSize();
+}
+
+function recalculateRelativeSize(){
+    relativeSize = canvas.width/canvas.clientWidth;
+    console.log(relativeSize);
+}
+
+function draw(e){
+    if (drawing){
+        ctx.beginPath();
+        ctx.arc(e.offsetX*relativeSize,e.offsetY*relativeSize,brushSize,0,2*Math.PI);
+        ctx.fill();
     }
 }
